049fa7946148e540dc82ffc75e47c875066a0b88
chipcHE163829 <chipche163829@fpt.edu.vn>
2024-08-29 21:26:38 +0700

    [update]
    push

---
+++ BE.Api/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using BE.Application.Services.Users.Commands.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public UserController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+
+		//Insert User to Database
+		[HttpPost]
+		public async Task<IActionResult> InsertUser([FromBody] InsertUserCommand command)
+		{
+			//if (!ModelState.IsValid)
+			//{
+			//	return BadRequest(ModelState);
+			//}
+
+			var result = await _mediator.Send(command);
+
+			return Ok();
+		}
+	}
+}
